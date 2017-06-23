6f306a1d1eb230ff2746b73f371490d54aa721ab
Sang <https://github.com/sangchunahn>
2017-06-23 14:41:54 -0600

    created render list function

--- youtube-app/src/components/video/Video_list_item.js
+++ youtube-app/src/components/video/Video_list_item.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class VideoListItem extends Component {
+  renderList() {
+    return (
+      <div>
+        Video List
+      </div>
+    )
+  }
   render() {
     return (
       <div>
