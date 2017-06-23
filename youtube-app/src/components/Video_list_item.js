e037deb0923df324adf1fc051c76f6c12fe6cb04
Sang <https://github.com/sangchunahn>
2017-06-23 11:40:46 -0600

    added component class to each file

---
+++ youtube-app/src/components/Video_list_item.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoListItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default VideoListItem;
